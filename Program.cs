84916a0fd8c0c0b8ca65e05b2f6be2e2454dca32
Emmanuel Ochoa <eorivera95@gmail.com>
2024-03-11 19:12:11 -0600

    finished wheater app

--- Program.cs
+++ Program.cs
@@ -1,2 +1,111 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+namespace Program // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherData weatherData = new WeatherData();
+            CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+
+            weatherData.SetMeasurements(80, 65, 30.4f);
+            weatherData.SetMeasurements(82, 70, 29.2f);
+            weatherData.SetMeasurements(78, 90, 29.2f);
+
+        }
+    }
+
+    public interface ISubject
+    {
+        public void RegisterObserver(IObserver o);
+        public void RemoveObserver(IObserver o);
+        public void NotifyObservers();
+    }
+
+    public interface IObserver
+    {
+        public void Update(float temp, float humidity, float pressure);
+    }
+
+    public interface IDisplayElement
+    {
+        public void Display();
+    }
+
+    public class WeatherData : ISubject
+    {
+        private ArrayList observers;
+        private float temperature;
+        private float humidity;
+        private float pressure;
+
+        public WeatherData()
+        {
+            observers = new ArrayList();
+
+        }
+        public void NotifyObservers()
+        {
+            foreach (IObserver observer in observers)
+            {
+                observer.Update(temperature, humidity, pressure);
+            }
+
+        }
+
+        public void RegisterObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            int i = observers.IndexOf(o);
+            if (i >= 0)
+            {
+                observers.Remove(i);
+            }
+        }
+
+        public void MeasurementsChanged()
+        {
+            NotifyObservers();
+        }
+
+        public void SetMeasurements(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+            MeasurementsChanged();
+        }
+    }
+
+    public class CurrentConditionsDisplay : IObserver, IDisplayElement
+    {
+        private float temperature;
+        private float humidity;
+        private ISubject weatherData;
+
+        public CurrentConditionsDisplay(ISubject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+        public void Display()
+        {
+            System.Console.WriteLine("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+        }
+
+        public void Update(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            Display();
+        }
+    }
+
+
+}
